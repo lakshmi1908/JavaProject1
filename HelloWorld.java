04294f839e38e43eddc8fe58903ac001e6c6a03f
lakshmi1908 <maha1982002@gmail.com>
2022-09-24 10:59:02 +0530

    modified file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome Lab Assessment Term2!");
+		System.out.println("Term2 Lab Assessment!");
 	}
 
 }
